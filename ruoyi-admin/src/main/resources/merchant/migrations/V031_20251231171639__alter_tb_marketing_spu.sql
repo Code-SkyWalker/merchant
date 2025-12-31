399fec7c19401306f0c1e8dfc426c329804e8b0e
Code-SkyWalker <916420794@qq.com>
2025-12-31 17:45:10 +0800

    feat(database): 优化商户相关表结构与索引设计
    - 修改 tb_category 表字段属性，添加 template_id 字段
    - 重构 tb_marketing_spu 表，删除 rule 相关字段及索引，新增 marketing_id 字段及唯一索引
    - 调整 tb_merchant_certify 表，删除唯一索引，更改 approval_status 默认值
    - 精简 tb_merchant_shipping_area 表，删除多个租户及创建更新相关字段
    - 调整 tb_merchant_shipping_template 表，替换 tenantId 字段为 tenant_id，添加字段注释
    - 为 tb_sku 表添加 spu_id 字段索引
    - 优化 tb_spu 表索引，添加复合索引 tenant_id_merchant_id 和 create_time_id 索引

--- ruoyi-admin/src/main/resources/merchant/migrations/V031_20251231171639__alter_tb_marketing_spu.sql
+++ ruoyi-admin/src/main/resources/merchant/migrations/V031_20251231171639__alter_tb_marketing_spu.sql
@@ -4,8 +4,6 @@ ALTER TABLE `merchant_nearby`.`tb_marketing_spu` DROP COLUMN `rule_type`;
 
 ALTER TABLE `merchant_nearby`.`tb_marketing_spu` DROP INDEX `idx_uq_rule_id_spu_id`;
 
-ALTER TABLE `merchant_nearby`.`tb_marketing_spu` DROP INDEX `idx_rule_type`;
-
 ALTER TABLE `merchant_nearby`.`tb_marketing_spu` MODIFY COLUMN `id` bigint NOT NULL COMMENT '关联主键' FIRST;
 
 ALTER TABLE `merchant_nearby`.`tb_marketing_spu` ADD COLUMN `marketing_id` bigint NOT NULL COMMENT '活动主键' AFTER `id`;
