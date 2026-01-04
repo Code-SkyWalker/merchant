1176de2951ec0a3ac54ccb48d4bfef236b1468f1
Code-SkyWalker <916420794@qq.com>
2026-01-04 15:59:22 +0800

    feat(commodity): 为新商品创建命令生成唯一ID
    - 引入SnowflakeIdGenerator用于生成唯一标识
    - 修改SpuCreateExe执行方法，设置cmd的ID为生成的唯一ID
    - 确保保存的商品实体拥有唯一标识符
    - 优化商品创建流程，防止ID冲突问题

--- ruoyi-modules/ruoyi-merchant-nearby/ruoyi-merchant-nearby-app/src/main/java/org/dromara/merchant/app/commodity/executor/SpuCreateExe.java
+++ ruoyi-modules/ruoyi-merchant-nearby/ruoyi-merchant-nearby-app/src/main/java/org/dromara/merchant/app/commodity/executor/SpuCreateExe.java
@@ -1,6 +1,7 @@
 package org.dromara.merchant.app.commodity.executor;
 
 import lombok.RequiredArgsConstructor;
+import org.dromara.common.core.utils.SnowflakeIdGenerator;
 import org.dromara.merchant.client.commodity.dto.data.command.SpuCreateCmd;
 import org.dromara.merchant.domain.commodity.gateway.ISpuGateway;
 import org.dromara.merchant.domain.commodity.model.Spu;
@@ -20,6 +21,7 @@ public class SpuCreateExe {
     private final SpuConvertor spuConvertor;
 
     public Long execute(SpuCreateCmd cmd) {
+        cmd.setId(SnowflakeIdGenerator.generateId());
         Spu spu = spuConvertor.toEntity(cmd);
         if (spuGateway.save(spu)) return spu.getId();
         return null;
